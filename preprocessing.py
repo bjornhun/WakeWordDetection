9e0b242f4e9ce0704c5ef3ffb3b90b7645613d38
Bjornar Hunshamar <bjornarhuns@gmail.com>
2017-11-08 15:46:03 +0100

    First version of preprocessing finished. All files are treated equally (900 msec windows, 100 msec steps). Data stored in .npy file.

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from python_speech_features import mfcc
+import os
 
 def set_length(x, cutoff=14400):
     '''Gives segments appropriate length by either slicing or zero-padding.
@@ -14,10 +15,12 @@ def set_length(x, cutoff=14400):
         zeros = cutoff - num_samples
         return np.append(x, [0]*zeros)
 
-def get_coefficients(x, frame_size=14400, step_size=1600):
+def get_coefficients(x, label, frame_size=14400, step_size=1600):
     '''Partitions sound signal into frames.
     Default is 900 msec frames with 100 msec steps.
-    Returns MFCC coefficients of the input signal as 3D np array.'''
+    Returns MFCC coefficients of the input signal as numpy array:
+    [[[2D data], label], [[2D data], label], ... , [[2D data], label]]
+    '''
     
     if len(x) < 14400:
         x = set_length(x)
@@ -26,14 +29,35 @@ def get_coefficients(x, frame_size=14400, step_size=1600):
     frames = []
     for i in range (0, x_length-frame_size+1, step_size):
         coeff = mfcc(x[i:(i+frame_size)])
-        frames.append(coeff)
+        frames.append([coeff, label])
     return np.asarray(frames)
 
-if __name__ == '__main__':
-    # Fs = 16000, samples = 53248
-    '''fs, x = wavfile.read("data/train/f029_04_067.wav")
-    coeff = mfcc(x)
-    print(coeff.shape)'''
+def preprocess():
+    '''Reads all training data and gets MFCC coefficients.
+    Returns np array with all MFCC data + label.
+    '''
+    filepath = data_path = os.getcwd() + "\\data\\train\\"
+    os.chdir(filepath)
+    files = os.listdir()
+    num_of_files = len(files)
 
-    for i in range(0, 1, 10):
-        print(i)
+    data = []
+    i = 1
+
+    for f in files:
+        if f.endswith("042.wav"):
+            label = 1
+        else:
+            label = 0
+
+        fs, x = wavfile.read(f)
+        data.append(get_coefficients(x, label))
+        print(str(i) + "/" + str(num_of_files) + " files preprocessed.")
+        i+=1
+    
+    return np.asarray(data)
+
+
+if __name__ == '__main__':
+    #data = preprocess()
+    #np.save("data.npy", data)
--- testPreprocessing.py
+++ testPreprocessing.py
@@ -1,38 +1,36 @@
 import unittest
 import random
 import preprocessing
+from scipy.io import wavfile
 
 class TestPreprocessing(unittest.TestCase):
     def setUp(self):
         self.seg_length = 14400
-        self.seg = random.choices(range(-100, 100), k=self.seg_length)
-        self.short_seg = random.choices(range(-100, 100), k=(self.seg_length - random.randint(0, self.seg_length)))
-        self.long_seg = random.choices(range(-100, 100), k=(self.seg_length + random.randint(0, self.seg_length)))
+        fs, x = wavfile.read("data/train/f029_04_067.wav")        
+        self.seg = x[:14400]
+        self.short_seg = x[:10000]
+        self.long_seg = x
 
 
     def test_set_length(self):
+        short_seg = preprocessing.set_length(self.short_seg)
+        long_seg = preprocessing.set_length(self.long_seg)
 
-        short_seg_uni = preprocessing.set_length(self.short_seg)
-        long_seg_uni = preprocessing.set_length(self.long_seg)
-
-        self.assertEqual(len(short_seg_uni), self.seg_length)
-        self.assertEqual(len(long_seg_uni), self.seg_length)
-        self.assertEqual(len(long_seg_uni), len(short_seg_uni))
+        self.assertEqual(len(short_seg), self.seg_length)
+        self.assertEqual(len(long_seg), self.seg_length)
 
 
     def test_get_coefficients(self):
-        frames = preprocessing.get_coefficients(self.seg)
-        frames_short = preprocessing.get_coefficients(self.short_seg)
-        frames_long = preprocessing.get_coefficients(self.long_seg)
+        frames = preprocessing.get_coefficients(self.seg, 0)
+        frames_short = preprocessing.get_coefficients(self.short_seg, 0)
+        frames_long = preprocessing.get_coefficients(self.long_seg, 0)
         
-        self.assertEqual(frames.shape[1], 14400)
-        self.assertEqual(frames_short.shape[1], 14400)
-        self.assertEqual(frames_long.shape[1], 14400)
-        self.assertEqual(frames.shape[0], 1)
-        self.assertEqual(frames_short.shape[0], 1)
-        self.assertEqual(frames_long.shape[0], 1 + (len(self.long_seg) - 14400) // 1600)
-        self.assertListEqual(list(frames[0]), list(self.seg[:14400]))
-        self.assertListEqual(list(frames_long[0]), list(self.long_seg[:14400]))
+        self.assertEqual(frames[0][0].shape[1], 13)
+        self.assertEqual(frames_short[0][0].shape[1], 13)
+        self.assertEqual(frames_long[0][0].shape[1], 13)
+        self.assertEqual(frames[0][1], 0)
+        self.assertEqual(frames_short[0][1], 0)
+        self.assertEqual(frames_long[0][1], 0)
 
 
 if __name__ == '__main__':
