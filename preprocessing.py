2490dfe0c19736679f027d94b83c431130aa8e04
Bjornar Hunshamar <bjornarhuns@gmail.com>
2017-11-09 10:48:00 +0100

    Split features and labels into seperate lists

--- preprocessing.py
+++ preprocessing.py
@@ -9,7 +9,7 @@ def set_length(x, cutoff=14400):
     '''Gives segments appropriate length by either slicing or zero-padding.
     Default length/cutoff is 900 msec.
     Returns input signal with modified length.'''
-    
+
     num_samples = len(x)
     if num_samples > cutoff:
         return x[:cutoff]
@@ -28,11 +28,13 @@ def get_coefficients(x, label, frame_size=14400, step_size=1600):
         x = set_length(x)
     
     x_length = len(x)
-    frames = []
+    data = []
+    labels = []
     for i in range (0, x_length-frame_size+1, step_size):
         coeff = mfcc(x[i:(i+frame_size)])
-        frames.append([coeff, label])
-    return frames
+        data.append(coeff)
+        labels.append(label)
+    return data, labels
 
 def preprocess(filepath):
     '''Reads all data from filepath and gets MFCC coefficients.
@@ -41,9 +43,10 @@ def preprocess(filepath):
     files = os.listdir(filepath)
     num_of_files = len(files)
 
-    data = []
-    i = 1
+    X = []
+    y = []
 
+    count = 1
     for f in files:
         if f.endswith("042.wav"):
             label = 1
@@ -51,11 +54,14 @@ def preprocess(filepath):
             label = 0
 
         fs, x = wavfile.read(filepath + f)
-        data.extend(get_coefficients(x, label))
-        print(str(i) + "/" + str(num_of_files) + " files preprocessed.")
-        i+=1
+        data, labels = get_coefficients(x, label)
+        X.extend(data)
+        y.extend(labels)
+
+        print(str(count) + "/" + str(num_of_files) + " files preprocessed.")
+        count+=1
     
-    return data
+    return X, y
 
 def get_train_data():
     '''Reads and returns preprocessed training data from pickle if it exists.
@@ -63,9 +69,9 @@ def get_train_data():
 
     if os.path.isfile("data/train.pickle"):
         return read_pickle("data/train.pickle")
-    data = preprocess("data/train/")
-    to_pickle(data, "data/train.pickle")
-    return data
+    X_train, y_train = preprocess("data/train/")
+    to_pickle([X_train, y_train], "data/train.pickle")
+    return [X_train, y_train]
 
 def get_test_data():
     '''Reads and returns preprocessed test data from pickle if it exists.
@@ -73,9 +79,9 @@ def get_test_data():
 
     if os.path.isfile("data/test.pickle"):
         return read_pickle("data/test.pickle")
-    data = preprocess("data/test/")
-    to_pickle(data, "data/test.pickle")
-    return data
+    X_test, y_test = preprocess("data/test/")
+    to_pickle([X_test, y_test], "data/test.pickle")
+    return [X_test, y_test]
 
 if __name__ == '__main__':
     get_train_data()
