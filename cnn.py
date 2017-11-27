8f60f4552e0d82c2a539cad34069860f59458d52
Bjornar Hunshamar <bjornarhuns@gmail.com>
2017-11-27 14:54:58 +0100

    Funker

--- cnn.py
+++ cnn.py
@@ -22,40 +22,25 @@ import custom_metrics
 
 batch_size = 128
 epochs = 20
+filename = "cnn_crazy.h5"
 
 # Read data
-X_train, y_train = read_pickle("data_ws/train.pickle")
-X_test, y_test = read_pickle("data_ws/test.pickle")
-#X_rec, y_rec = read_pickle("data/rec.pickle")
+X_train, y_train = read_pickle("data/train.pickle")
+X_test, y_test = read_pickle("data/test.pickle")
+X_rec, y_rec = read_pickle("data/rec.pickle")
 
-num_neg = len([x for x in y_train if x==0])/len(y_train)
+'''num_neg = len([x for x in y_train if x==0])/len(y_train)
 num_pos = len([x for x in y_train if x==1])/len(y_train)
 
-class_weight = {0:num_pos, 1:num_neg}
-
-'''
-X_train = np.asarray(X_train)
-X_test = np.asarray(X_test)
-'''
+class_weight = {0:num_pos, 1:num_neg}'''
 
 (num_train, rows, columns) = X_train.shape
 num_test = X_test.shape[0]
-#num_rec = X_rec.shape[0]
-
-'''
-# Normalize
-for coeff in X_train:
-    coeff += np.abs(coeff.min())
-    coeff /= coeff.max()
-
-for coeff in X_test:
-    coeff += np.abs(coeff.min())
-    coeff /= coeff.max()
-'''
+num_rec = X_rec.shape[0]
 
 X_train = X_train.reshape(num_train, rows, columns, 1)
 X_test = X_test.reshape(num_test, rows, columns, 1)
-#X_rec = X_rec.reshape(num_rec, rows, columns, 1)
+X_rec = X_rec.reshape(num_rec, rows, columns, 1)
 
 def train_model(X_train, y_train, path):
     # Define model
@@ -66,7 +51,11 @@ def train_model(X_train, y_train, path):
                     input_shape=(rows, columns, 1),
                     kernel_initializer='he_uniform'))
     model.add(Conv2D(32, 
-                    (3, 3),
+                    (5, 3),
+                    activation='relu',
+                    kernel_initializer='he_uniform'))
+    model.add(Conv2D(32, 
+                    (5, 3),
                     activation='relu',
                     kernel_initializer='he_uniform'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -89,18 +78,17 @@ def train_model(X_train, y_train, path):
             batch_size=batch_size,
             epochs=epochs,
             verbose=1,
-            validation_split=0.1,
-            class_weight=class_weight)
+            validation_split=0.1)
 
     # Save model
     model.save(path)
 
     return model
 
-train_model(X_train, y_train, "models/cnn_ws_b.h5")
+train_model(X_train, y_train, "models/" + filename)
 
 # Read model
-model = load_model('models/cnn_ws_b.h5', custom_objects={'fmeasure': custom_metrics.fmeasure})
+model = load_model('models/' + filename, custom_objects={'fmeasure': custom_metrics.fmeasure})
           
 # Evaluate model
 print("Test data:")
@@ -108,7 +96,7 @@ score = model.evaluate(X_test, y_test, verbose=1)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 print('f-score:', score[2])
-'''
+
 print("Female data:")
 score = model.evaluate(X_test[:1000], y_test[:1000], verbose=1)
 print('Test loss:', score[0])
@@ -131,4 +119,3 @@ print("predictions:")
 print([int(round(x[0])) for x in model.predict(X_rec)])
 print("true:")
 print(y_rec)
-'''
